476647a70105064803f35526c702ea33f3a726a7
hitode909 <hitode909@gmail.com>
2010-10-21 17:56:48 +0900

    slice

--- lib/MaRo/Driver/Net/Cassandra.pm
+++ lib/MaRo/Driver/Net/Cassandra.pm
@@ -14,7 +14,7 @@ __PACKAGE__->mk_accessors(
 sub new {
     my ($class, $database) = @_;
     croak "database is required" unless $database;
-    my $cassandra = Net::Cassandra->new( hostname => $database->host );
+    my $cassandra = Net::Cassandra->new( hostname => $database->host, port => $database->port );
     my $client    = $cassandra->client;
     $class->SUPER::new({client => $client});
 }
@@ -25,7 +25,7 @@ sub set {
 
     eval {
         $self->client->insert(
-            'Keyspace1',
+            $arg->{key_space},
             $arg->{key},
             Net::Cassandra::Backend::ColumnPath->new(
                 {
@@ -37,7 +37,7 @@ sub set {
           );
     };
     die $@->why if $@;
-    $value;
+    1;
 }
 
 sub get {
@@ -55,11 +55,69 @@ sub get {
             Net::Cassandra::Backend::ConsistencyLevel::QUORUM
           );
         $value = $what->column->value;
-        my $timestamp = $what->column->timestamp;
     };
-    return if $@ && $@->isa('Net::Cassandra::Backend::NotFoundException');
+    if ($@->isa('Net::Cassandra::Backend::NotFoundException')) {
+        die 'not found';
+    }
     die $@->why if $@;
     $value;
+}
+
+# 何を返すべきか．．．．．．．．．
+sub slice {
+    my ($self, $arg) = @_;
+    my $what;
+    eval {
+        $what = $self->client->get_slice(
+            $arg->{key_space},
+            $arg->{key},
+            Net::Cassandra::Backend::ColumnParent->new(
+                {
+                    column_family => $arg->{column_family}
+                }
+            ),
+            Net::Cassandra::Backend::SlicePredicate->new(
+                {
+                    slice_range => Net::Cassandra::Backend::SliceRange->new(
+                        {
+                            start => $arg->{start} || '', finish => '', count => $arg->{count} || 100 }
+                )
+                }
+            ),
+            Net::Cassandra::Backend::ConsistencyLevel::QUORUM
+          );
+    };
+    die $@->why if $@;
+    [map { [$_->column->name, $_->column->value]} @$what];
+}
+
+sub slice_as_hash {
+    my ($self, $arg) = @_;
+    my $values = $self->slice($arg);
+
+    my $result = {};
+    for (@$values) {
+        my ($k, $v) = @$_;
+        $result->{$k} = $v;
+    }
+    $result;
+}
+
+sub delete {
+    my ($self, $arg) = @_;
+    eval {
+        $self->client->remove(
+            $arg->{key_space},
+            $arg->{key},
+            Net::Cassandra::Backend::ColumnPath->new(
+                {
+                    column_family => $arg->{column_family}, column => $arg->{column} }
+            ),
+            time
+        );
+    };
+    die $@->why if $@;
+    1;
 }
 
 sub validate_key_arg {
